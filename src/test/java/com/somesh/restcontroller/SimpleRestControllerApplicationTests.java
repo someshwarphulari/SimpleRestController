68345b4e94b55e39b2b815b5ca97f39b1043fa98
someshwarphulari <someshwarphulari@gmail.com>
2024-04-21 03:10:32 +0530

    Simple Controller setup files

---
+++ src/test/java/com/somesh/restcontroller/SimpleRestControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somesh.restcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
